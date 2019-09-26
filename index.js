3193165ee5db2d8f28d356cc1a47cceb3d6f2c88
Anthony Rurup <tcrurup@gmail.com>
2019-09-26 22:24:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ function logDriversByHometown(drivers, location){
 
 function driversByRevenue(drivers){
   return drivers.sort(function(a,b){
-    return a.revenue < b.revenue;
+    return a.revenue > b.revenue;
   });
 }
