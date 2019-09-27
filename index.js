98f605d461bd9463091c6995925b9a4da4a40417
Anthony Rurup <tcrurup@gmail.com>
2019-09-27 01:00:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution in this file!
+
+function logDriverNames(drivers){
+  drivers.forEach(function(driver){
+    console.log(driver.name);
+  });
+}
+
+function logDriversByHometown(drivers, location){
+  
+  drivers.forEach(function(driver){
+    if(driver.hometown === location){
+      console.log(driver.name);
+    }
+  });
+}
+
+function driversByRevenue(drivers){
+  const copiedArray = [...drivers];
+  return copiedArray.sort(function(a,b){
+    return a.revenue > b.revenue;
+  });
+}
+
+function driversByName(drivers){
+  const copiedArray = [...drivers];
+  return copiedArray.sort(function(a,b){
+    return a.name > b.name;
+  });
+}
+
+function totalRevenue(drivers){
+  return drivers.reduce(function(sum, driver){
+    return sum + driver.revenue;
+  }, 0);
+}
+
+function averageRevenue(drivers){
+   return totalRevenue(drivers)/drivers.length;
+}
